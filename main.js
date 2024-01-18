04707d16be1475136649862b0a68d842cec6c3a4
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-18 16:50:53 +0100

    feat: countdown in HH:MM:SS format

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const clockHtml = document.getElementById('clockhtml')
 
 //associamo a questa variabile il giorno e l'orario di domani (deadline) in millisecondi
-const targetData = new Date("Jan 19, 2024, 9:30:0").getTime();
+const targetData = new Date("Jan 19, 2024, 09:30:00").getTime();
 
 //variabile che conterrà la giornata e l'orario attuale, sempre in millisecondi
 let today;
@@ -10,18 +10,25 @@ let today;
 let countDown;
 
 //funzione temporale (attualmente senza parametri di tempo per rendere più visibile lo scalare dei millisecondi)
-const clockJS = setInterval(timer)
+const clockJS = setInterval(timer, 1000)
 
 //funzione timer per il conteggio dei millisecondi
 function timer(){
     
+
     if(countDown < 0){
         clearInterval(clockJS);
         clockHtml.innerHTML = "Tempo della correzione!"
     }else{
         today = new Date().getTime();
         countDown = targetData - today;
-        clockHtml.innerHTML = countDown;
+        
+        countDown = new Date(countDown);
+        let dateTransform = new Date(countDown)        
+        console.log(dateTransform);
+        
+        clockHtml.innerHTML = `${dateTransform.getHours()}:${dateTransform.getMinutes()}:${dateTransform.getSeconds()}`;
+
     }
 }
 
