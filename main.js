294789b0377119c78796c644f5150259cc09b65e
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-19 09:55:50 +0100

    fix: style

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const clockHtml = document.getElementById('clockhtml')
 //associamo a questa variabile il giorno e l'orario di domani (deadline) in millisecondi
 const targetData = new Date("Jan 19, 2024, 09:30:00").getTime();
 
+
 //variabile che conterrà la giornata e l'orario attuale, sempre in millisecondi
 let today;
-
 //variabile che conterrà la differenza di tempo in millisecondi tra la giornata di oggi e la giornata target
-let countDown;
+let countDown = 0;
 
 //funzione temporale (attualmente senza parametri di tempo per rendere più visibile lo scalare dei millisecondi)
 const clockJS = setInterval(timer, 1000)
@@ -15,10 +15,10 @@ const clockJS = setInterval(timer, 1000)
 //funzione timer per il conteggio dei millisecondi
 function timer(){
     
-
-    if(countDown < 0){
+    if(countDown < 1){
         clearInterval(clockJS);
         clockHtml.innerHTML = "Tempo della correzione!"
+
     }else{
         //prendiamo la data di oggi in millisecondi, la sottraiamo al valore in millisecondi del giorno target e il risultato lo trasformiano nuovamente in formato originale.
         today = new Date().getTime();
